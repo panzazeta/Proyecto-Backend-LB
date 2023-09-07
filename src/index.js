af9d2e95bc1f6dc421ad6c56632868b340280221
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-09-07 20:33:45 -0300

    add chat

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import path from "path";;
+import path from "path";
 import mongoose from 'mongoose'
 import userRouter from './routes/users.routes.js'
 import productRouter from './routes/products.routes.js'
@@ -11,17 +11,43 @@ import { Server } from "socket.io";
 const app = express()
 const PORT = 4000
 
+const serverExpress = app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
+
 mongoose.connect('mongodb+srv://lucasbenielli:785JahPnIQZk8SOs@clusterlb.d50iram.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('BDD conectada'))
     .catch(() => console.log('Error en conexion a BDD'))
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "./views"));
+app.use("/static", express.static(path.join(__dirname, "/public")));
+
+//Server Socket.io
+const io = new Server(serverExpress);
 
 app.use('/api/users', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter);
 // app.use('/api/messages', messageRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server on Port ${PORT}`)
-})
+// app.get("/", async (req,res) => {
+//     const productList = await productsManager.getProducts();
+//     res.render("index", {
+//             css: "index.css",
+//             title: "Index",
+//             products: productList
+//     });
+// });
+
+// app.get("/static", (req, res) => {
+//     res.render("realTimeProducts", {
+//         css: "static.css",
+//         title: "Products",
+//         js: "realTimeProducts.js"
+//     }) 
+// });
