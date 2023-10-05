c3111538811fb58514672e75b6286d8d3440b406
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-10-05 10:40:55 -0300

    add code

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,5 +1,6 @@
 import local from 'passport-local' //Importo la estrategia
 import GithubStrategy from 'passport-github2';
+import jwt from 'passport-jwt'
 import passport from 'passport'
 import { createHash, validatePassword } from '../utils/bycript.js'
 import { userModel } from '../models/users.models.js'
@@ -7,6 +8,29 @@ import { userModel } from '../models/users.models.js'
 const LocalStrategy = local.Strategy
 
 const initializePassport = () => {
+
+    const cookieExtractor = req => {
+        const token = req.cookies.jwtCookie ? req.cookies.jwtCookie : {}
+
+        console.log("cookieExtractor", token)
+
+        return token
+
+    }
+
+    passport.use('jwt', new JWTStrategy({
+        jwtFromRequest: ExtractJWT.fromExtractors([cookieExtractor]), //El token va a venir desde cookieExtractor
+        secretOrKey: process.env.JWT_SECRET
+    }, async (jwt_payload, done) => { //jwt_payload = info del token (en este caso, datos del cliente)
+        try {
+            console.log("JWT", jwt_payload)
+            return done(null, jwt_payload)
+        } catch (error) {
+            return done(error)
+        }
+
+    }))
+
 
     passport.use('register', new LocalStrategy(
         { passReqToCallback: true, usernameField: 'email' }, async (req, username, password, done) => {
